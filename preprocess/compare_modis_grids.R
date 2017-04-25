980369e9e94e55fbaf007125be6cb230e59eeb47
Ian Hough <142520+ihough@users.noreply.github.com>
2017-04-25 14:58:19 +0200

    Designate a single reference grid if all MODIS are aligned

--- preprocess/compare_modis_grids.R
+++ preprocess/compare_modis_grids.R
@@ -5,44 +5,64 @@ library("raster")
 
 file.path("~", "data", "r") %>% path.expand %>% setwd
 
-aqua_lst = raster("modis_aqua_lst_grid.tif")
-aqua_ndvi = raster("modis_aqua_ndvi_grid.tif")
-terra_lst = raster("modis_terra_lst_grid.tif")
-terra_ndvi = raster("modis_terra_ndvi_grid.tif")
+# Load the MODIS Aqua and Terra LST and NDVI grid rasters
+products <-
+  c(
+    "aqua_lst",
+    "aqua_ndvi",
+    "terra_lst",
+    "terra_ndvi"
+  )
 
-print_diff <- function(a, b) {
-  if (all.equal(a, b, tolerance = 0, res = TRUE, orig = TRUE, showwarning = FALSE)) {
-    print("  Identical")
-  } else {
-    (origin(a) - origin(b)) %>%
-      max %>%
-      paste("  Max origin difference : ", .) %>%
-      print
-    (res(a) - res(b)) %>%
-      max %>%
-      paste("  Max resolution difference : ", .) %>%
-      print
-    (coordinates(a) - coordinates(b)) %>%
-      max %>%
-      paste("  Max xy difference : ", .) %>%
-      print
-  }
+grids <-
+  sapply(products, function(p) {
+    paste("modis", p, "grid.tif", sep = "_") %>% raster
+  })
+
+# Return the max difference between the origin, resolution, and coordinates of two grids
+difference <- function(a, b) {
+  d <-
+    c(
+      max(origin(a) - origin(b)),
+      max(res(a) - res(b)),
+      max(coordinates(a) - coordinates(b))
+    )
+  names(d) <- c("origin", "resolution", "coordinates")
+  d
 }
 
+# Calculate misalignment between Aqua and Terra LST grids
+print("Difference in alignment between MODIS grids:")
+
 print("Aqua LST vs Terra LST")
-print_diff(aqua_lst, terra_lst)
+lst_vs_lst <- difference(grids$aqua_lst, grids$terra_lst)
+print(lst_vs_lst)
 
-print("")
-
+# Calculate misalignment between Aqua and Terra NDVI grids
 print("Aqua NDVI vs Tera NDVI")
-print_diff(aqua_ndvi, terra_ndvi)
+ndvi_vs_ndvi <- difference(grids$aqua_ndvi, grids$terra_ndvi)
+print(ndvi_vs_ndvi)
 
-print("")
-
+# Calculate misalignment between Aqua LST and NDVI grids
 print("Aqua LST vs Aqua NDVI")
-print_diff(aqua_lst, aqua_ndvi)
+aqua_lst_vs_ndvi <- difference(grids$aqua_lst, grids$aqua_ndvi)
+print(aqua_lst_vs_ndvi)
 
-print("")
-
+# Calculate misalignment between Terra LST and NDVI grids
 print("Terra LST vs Terra NDVI")
-print_diff(terra_lst, terra_ndvi)
+terra_lst_vs_ndvi <- difference(grids$terra_lst, grids$terra_ndvi)
+print(terra_lst_vs_ndvi)
+
+# Combine the differences into a data frame
+diffs <-
+  rbind(lst_vs_lst, ndvi_vs_ndvi, aqua_lst_vs_ndvi, terra_lst_vs_ndvi) %>%
+  as.data.frame
+
+# If the grids are aligned to within 1 mm, use the aqua lst grid as the reference
+if (max(diffs$coordinates) < 0.001) {
+  print("Grids are aligned to within 1 mm")
+  print("Saving aqua lst grid as 'modis_grid.rds'")
+  file.copy("modis_aqua_lst_grid.rds", "modis_grid.rds")
+} else {
+  print(paste("Grids are misaligned by up to", max(diffs$coordinates), "m"))
+}
