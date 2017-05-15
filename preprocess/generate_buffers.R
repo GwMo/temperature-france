8da69da0e75b54013a57e8c43081eac74b8735d6
Ian Hough <142520+ihough@users.noreply.github.com>
2017-05-15 11:22:33 +0200

    Refactor buffer creation

--- preprocess/generate_buffers.R
+++ preprocess/generate_buffers.R
@@ -1,4 +1,4 @@
-# Create buffers around the points of the reference grid
+# Generate buffers around the points of the reference grid
 
 library("magrittr") # %>% pipe-like operator
 library("sp")       # classes and methods for spatial data
@@ -8,11 +8,14 @@ library("parallel") # parallel computation
 
 print("Generating buffers around the reference grid points")
 
-# Set working directory
-file.path("~", "data", "r") %>% path.expand %>% setwd
+# Set directories
+base_dir <- file.path("~", "temperature-france") %>% path.expand
+buffer_dir <- file.path(base_dir, "buffers")
+dir.create(buffer_dir)
+setwd(buffer_dir)
 
 # Load the MODIS reference grid
-grid <- readRDS("modis_grid.rds")
+grid <- file.path(base_dir, "grids", "modis_grid.rds") %>% readRDS
 
 # Define EPSG:2154, the RGF93 / Lambert-93 projection
 # The official Lambert conformal conic projection for metropolitan France
@@ -20,11 +23,11 @@ epsg_2154 <- proj4string(grid)
 
 # Define EPSG:3035, the ETRS89 / LAEA Europe projection
 # A Europe-wide Lambert azimuthal equal area projection for statistical mapping
-epsg_3035 <- "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +no_defs"
+epsg_3035 <- "+proj=laea +lat_0=52 +lon_0=10 +x_0=4321000 +y_0=3210000 +ellps=GRS80 +units=m +no_defs"
 
 # Project the grid to EPSG:3035
 # Creating the buffers in EPSG:3035 ensures they have the same area
-grid_epsg_3035 <- spTransform(grid, epsg_3035)
+grid_3035 <- spTransform(grid, epsg_3035)
 
 # Generate and save 1 km and 3 km square buffers around each grid point
 distances <- c(1, 3)
@@ -33,9 +36,9 @@ mclapply(
   function(dist) {
     paste("Creating", dist, "km square buffer") %>% print
     buf <-
-      gBuffer(grid_epsg_3035, width = dist * 500, byid = TRUE, capStyle = "SQUARE") %>%
+      gBuffer(grid_3035, width = dist * 500, byid = TRUE, capStyle = "SQUARE") %>%
       spTransform(., epsg_2154)
-    filename <- paste("modis_grid_square_", dist, "km.rds", sep = "")
+    filename <- paste("modis_square_", dist, "km.rds", sep = "")
     paste("Saving", filename) %>% print
     saveRDS(buf, filename)
   },
@@ -49,11 +52,11 @@ mclapply(
   function(dist) {
     paste("Creating", dist, "km buffer") %>% print
     buf <-
-      gBuffer(grid_epsg_3035, width = dist * 500, byid = TRUE) %>%
+      gBuffer(grid_3035, width = dist * 500, byid = TRUE) %>%
       spTransform(., epsg_2154)
-    filename <- paste("modis_grid_buffer_", dist, "km.rds", sep = "")
+    filename <- paste("modis_round_", dist, "km.rds", sep = "")
     paste("Saving", filename) %>% print
     saveRDS(buf, filename)
   },
-  mc.cores = 4
+  mc.cores = 5
 )
