c3711de2b70a231e954e1283fa99fc1f27d403d1
Ian Hough <142520+ihough@users.noreply.github.com>
2017-06-30 19:40:28 +0200

    Script to extract MODIS NDVI data

---
+++ preprocess/extract_modis_ndvi.R
@@ -0,0 +1,128 @@
+# Extract MODIS NDVI data for the reference grid cells
+#
+# For each satellite (aqua / terra)
+# * For each month with data
+#   - Load and properly scale the monthly NDVI data
+#   - Load the basic QA data
+#   - Mask NDVI with the QA data to leave only highest quality pixels
+#   - Extract the data by the reference grid points
+#   - Add the MODIS grid id and save
+
+library(magrittr)  # %>% pipe-like operator
+library(parallel)  # parallel computation
+library(sp)        # classes and methods for spatial data
+library(rgdal)     # wrapper for GDAL and proj.4 to manipulate spatial data
+library(raster)    # methods to manipulate gridded spatial data
+library(gdalUtils) # extends rgdal and raster to manipulate HDF4 files
+
+data_dir <- file.path("~", "data") %>% path.expand
+model_dir <- file.path("~", "temperature-france") %>% path.expand
+output_dir <- file.path(model_dir, "data", "modis")
+dir.create(output_dir, showWarnings = FALSE)
+
+# Load helper functions
+file.path(model_dir, "helpers", "report.R") %>% source
+file.path(model_dir, "helpers", "get_ncores.R") %>% source
+
+# Load the reference grid
+report("Loading MODIS reference grid")
+grid <- file.path(model_dir, "grids", "modis_grid.rds") %>% readRDS
+
+# Detect the number of cores available
+ncores <- get_ncores()
+
+########################
+# MODIS Monthly 1km NDVI
+########################
+
+modis_dir <- file.path(data_dir, "modis")
+
+# Project the grid to match the MODIS data
+report("Projecting grid to MODIS sinusoidal")
+grid <- spTransform(grid, "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs")
+
+for(satellite in c("aqua", "terra")) {
+  paste("Processing MODIS", satellite, "NDVI data") %>% report
+
+  # Set the directory that contains the NDVI data for the satellite
+  ndvi_dir <-
+    file.path(modis_dir, satellite) %>%
+    list.files(., pattern = "^M.D13A3\\.006$", full.names = TRUE)
+
+  # Extract the NDVI data for each month
+  date_dirs <- list.dirs(ndvi_dir, recursive = FALSE)
+  mclapply(date_dirs, function(date_dir) {
+    # Parse the date from the directory name
+    date <- basename(date_dir) %>% as.Date
+
+    # Create a directory to hold the extracted data
+    year_dir <- format(date, "%Y") %>% file.path(output_dir, .)
+    dir.create(year_dir, showWarnings = FALSE)
+
+    paste("  Extracting", format(date, "%Y-%m"), "NDVI to", year_dir) %>% report
+
+    # Get all tiles for the date
+    tiles <- list.files(date_dir, full.names = TRUE, pattern = "\\.hdf$")
+
+    # Load monthly NDVI from the tiles, merge into a single raster, and crop
+    # to the reference grid. It's ok to merge rather than mosaic because MODIS
+    # tiles align without overlap.
+    ndvi <- lapply(tiles, function(tile) {
+      # Find the monthly NDVI dataset
+      datasets <- get_subdatasets(tile)
+      ndvi_dataset <- grepl("1 km monthly NDVI$", datasets) %>% datasets[.]
+
+      # Get the scale factor from the dataset metadata
+      ndvi_metadata <- gdalinfo(ndvi_dataset)
+      ndvi_scale_factor <-
+        grep("  scale_factor=", ndvi_metadata) %>%
+        ndvi_metadata[.] %>%
+        gsub("  scale_factor=(\\d+)", "\\1", .) %>%
+        as.integer
+
+      # Load the dataset as a raster and apply the scale factor
+      # NOTE: the scale factor is applied automatically when loading the raster
+      # but for some reason it is stored in the metadata as a divisor rather
+      # than a multiplier (i.e. 10000 rather than 0.0001). Thus to properly
+      # scale the data we need to divide by the square of the scale factor.
+      raster(ndvi_dataset) / (ndvi_scale_factor^2)
+    }) %>% do.call(merge, .) %>% crop(., grid, snap = "out")
+    # 7 seconds
+    # 9.03 MB
+
+    # Load, merge, and crop monthly NDVI QA data
+    qa <- lapply(tiles, function(tile) {
+      get_subdatasets(tile) %>%
+      .[grepl("1 km monthly pixel reliability$", .)] %>%
+      raster
+    }) %>% do.call(merge, .) %>% crop(., grid, snap = "out")
+    # 5 seconds
+    # 4.52 MB
+
+    # Mask NDVI with the QA data keeping only the highest-quality pixels
+    # TODO look at using lower-quality pixels as well
+    ndvi[qa != 0] <- NA
+
+    # Create a table to hold the extracted data
+    result <- data.frame("modis_grid_id" = grid$id, "date" = basename(date_dir))
+
+    # Extract the data by the reference grid points
+    # TODO test extracting data by buffers -> slower but more accurate, is the
+    # extra accuracy worth the effort?
+    result$ndvi <- extract(ndvi, grid)
+    # 2 seconds
+    # 10 MB
+
+    # Save the exctracted data
+    output_path <-
+      format(date, "%Y-%m") %>%
+      paste0("modis_grid_", satellite, "_ndvi_", ., ".rds") %>%
+      file.path(year_dir, .)
+    saveRDS(result, output_path)
+
+    # Return NULL to save memory
+    NULL
+  }, mc.cores = ncores)
+}
+
+report("Done")
