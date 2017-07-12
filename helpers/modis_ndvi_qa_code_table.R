f4aec883cfb10b7e6abbfe7b46b7372674b2bf83
Ian Hough <142520+ihough@users.noreply.github.com>
2017-07-12 19:25:34 +0200

    Include QA data in MODIS NDVI extraction

---
+++ helpers/modis_ndvi_qa_code_table.R
@@ -0,0 +1,60 @@
+# Return a table to aid in the interpretation of MODIS LST QA codes
+# Inspired by https://stevemosher.wordpress.com/2012/12/05/modis-qc-bits/
+modis_ndvi_qa_code_table <- function() {
+  # List all possible QA codes and their 16-bit big-endian binary notation
+  qa_codes <- lapply(0:65535, function(val) {
+    intToBits(val) %>% as.integer %>% .[16:1] %>% c(val, .)
+  }) %>% do.call(rbind, .) %>% as.data.frame
+  names(qa_codes) <- paste0("bit", 15:0) %>% c("code", .)
+
+  # Label the overall quality indicated by the QA bit values
+  qa_codes$quality[qa_codes$bit1 == 0 & qa_codes$bit0 == 0] <- "Good"     # Good quality, no need to check details
+  qa_codes$quality[qa_codes$bit1 == 0 & qa_codes$bit0 == 1] <- "Marginal" # Lower quality, see other bits for details
+  qa_codes$quality[qa_codes$bit1 == 1 & qa_codes$bit0 == 0] <- "Cloud"    # Pixel probably cloudy
+  qa_codes$quality[qa_codes$bit1 == 1 & qa_codes$bit0 == 1] <- "None"     # Pixel not produced for reason other than clouds
+
+  # Label the usefulness indicated by the QA bit values
+  # 1 = highest quality, 13 = lowest quality, NA = not useful
+  qa_codes$usefulness[qa_codes$bit5 == 0 & qa_codes$bit4 == 0 & qa_codes$bit3 == 0 & qa_codes$bit2 == 0] <-  1
+  qa_codes$usefulness[qa_codes$bit5 == 0 & qa_codes$bit4 == 0 & qa_codes$bit3 == 0 & qa_codes$bit2 == 1] <-  2
+  qa_codes$usefulness[qa_codes$bit5 == 0 & qa_codes$bit4 == 0 & qa_codes$bit3 == 1 & qa_codes$bit2 == 0] <-  3
+  qa_codes$usefulness[qa_codes$bit5 == 0 & qa_codes$bit4 == 0 & qa_codes$bit3 == 1 & qa_codes$bit2 == 1] <-  4
+  qa_codes$usefulness[qa_codes$bit5 == 0 & qa_codes$bit4 == 1 & qa_codes$bit3 == 0 & qa_codes$bit2 == 0] <-  5
+  qa_codes$usefulness[qa_codes$bit5 == 0 & qa_codes$bit4 == 1 & qa_codes$bit3 == 0 & qa_codes$bit2 == 1] <-  6
+  qa_codes$usefulness[qa_codes$bit5 == 0 & qa_codes$bit4 == 1 & qa_codes$bit3 == 1 & qa_codes$bit2 == 0] <-  7
+  qa_codes$usefulness[qa_codes$bit5 == 0 & qa_codes$bit4 == 1 & qa_codes$bit3 == 1 & qa_codes$bit2 == 1] <-  8
+  qa_codes$usefulness[qa_codes$bit5 == 1 & qa_codes$bit4 == 0 & qa_codes$bit3 == 0 & qa_codes$bit2 == 0] <-  9
+  qa_codes$usefulness[qa_codes$bit5 == 1 & qa_codes$bit4 == 0 & qa_codes$bit3 == 0 & qa_codes$bit2 == 1] <- 10
+  qa_codes$usefulness[qa_codes$bit5 == 1 & qa_codes$bit4 == 0 & qa_codes$bit3 == 1 & qa_codes$bit2 == 0] <- 11
+  qa_codes$usefulness[qa_codes$bit5 == 1 & qa_codes$bit4 == 0 & qa_codes$bit3 == 1 & qa_codes$bit2 == 1] <- 12
+  qa_codes$usefulness[qa_codes$bit5 == 1 & qa_codes$bit4 == 1 & qa_codes$bit3 == 0 & qa_codes$bit2 == 0] <- 13
+  qa_codes$usefulness[qa_codes$bit5 == 1 & qa_codes$bit4 == 1 & qa_codes$bit3 == 0 & qa_codes$bit2 == 1] <- NA
+  qa_codes$usefulness[qa_codes$bit5 == 1 & qa_codes$bit4 == 1 & qa_codes$bit3 == 1 & qa_codes$bit2 == 0] <- NA
+  qa_codes$usefulness[qa_codes$bit5 == 1 & qa_codes$bit4 == 1 & qa_codes$bit3 == 1 & qa_codes$bit2 == 1] <- NA
+
+  # QA bits 6-15 not currently used
+
+  # Clear the usefulness of pixels that were not produced
+  qa_codes$usefulness[qa_codes$quality == "None"] <- NA
+
+  # Note that many of the QA codes are likely never used. For example the QA
+  # code 2 (000010) means the pixel is most likely cloudy but is of very high
+  # quality. Cloudiness results in lower quality pixels so this code is probably
+  # never used. Similarly the QA code 27 (011011) means the pixel was not
+  # produced but is of medium usefulness. This is clearly nonsensical so we
+  # clear the usefulness but keep the code just in case.
+
+  # Return the table
+  qa_codes
+}
+
+#  code bit5 bit4 bit3 bit2 bit1 bit0  quality usefulness
+#     0    0    0    0    0    0    0     Good          1
+#     1    0    0    0    0    0    1 Marginal          1
+#     2    0    0    0    0    1    0    Cloud          1
+#     3    0    0    0    0    1    1     None         NA
+# ...
+# 65532    1    1    1    1    0    0     Good         NA
+# 65533    1    1    1    1    0    1 Marginal         NA
+# 65534    1    1    1    1    1    0    Cloud         NA
+# 65535    1    1    1    1    1    1     None         NA
--- preprocess/extract_modis_ndvi.R
+++ preprocess/extract_modis_ndvi.R
@@ -3,13 +3,12 @@
 # For each satellite (aqua / terra)
 # * For each month with data
 #   - Load and properly scale the monthly NDVI data
-#   - Load the basic QA data
-#   - Mask NDVI with the QA data to leave only highest quality pixels
-#   - Extract the data by the reference grid points
+#   - Extract the NDVI and QA data by the reference grid points
 #   - Add the MODIS grid id and save
 
 library(magrittr)  # %>% pipe-like operator
 library(parallel)  # parallel computation
+library(dplyr)     # data manipulation e.g. joining tables
 library(sp)        # classes and methods for spatial data
 library(rgdal)     # wrapper for GDAL and proj.4 to manipulate spatial data
 library(raster)    # methods to manipulate gridded spatial data
@@ -41,6 +40,12 @@ modis_dir <- file.path(data_dir, "modis")
 report("Projecting grid to MODIS sinusoidal")
 grid <- spTransform(grid, "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6371007.181 +units=m +no_defs")
 
+# Load a code table to aid in deciphering MODIS NDVI QA bit codes
+file.path(model_dir, "helpers", "modis_ndvi_qa_code_table.R") %>% source
+qa_codes <- modis_ndvi_qa_code_table()
+# 16 seconds
+
+# Extract Aqua and Terra data in sequence
 for(satellite in c("aqua", "terra")) {
   paste("Processing MODIS", satellite, "NDVI data") %>% report
 
@@ -49,22 +54,34 @@ for(satellite in c("aqua", "terra")) {
     file.path(modis_dir, satellite) %>%
     list.files(., pattern = "^M.D13A3\\.006$", full.names = TRUE)
 
-  # Extract the NDVI data for each month
+  # Extract the NDVI data for all months in parallel
   date_dirs <- list.dirs(ndvi_dir, recursive = FALSE)
   mclapply(date_dirs, function(date_dir) {
     # Parse the date from the directory name
-    date <- basename(date_dir) %>% as.Date
+    d <- basename(date_dir) %>% as.Date
 
     # Create a directory to hold the extracted data
-    year_dir <- format(date, "%Y") %>% file.path(output_dir, .)
+    year_dir <- format(d, "%Y") %>% file.path(output_dir, .)
     dir.create(year_dir, showWarnings = FALSE)
 
-    paste("  Extracting", format(date, "%Y-%m"), "NDVI to", year_dir) %>% report
+    format(d, "%Y-%m") %>%
+      paste("  Extracting", ., satellite, "monthly NDVI to", year_dir) %>%
+      report
+
+    # Create a template to hold the extracted data
+    result <- data.frame(
+      "modis_grid_id" = grid$id,
+      "date" = basename(date_dir),
+      "ndvi" = NA,
+      "ndvi_usefulness" = NA,
+      stringsAsFactors = FALSE
+    )
+    # 12.6 MB
 
     # Get all tiles for the date
     tiles <- list.files(date_dir, full.names = TRUE, pattern = "\\.hdf$")
 
-    # Load monthly NDVI from the tiles, merge into a single raster, and crop
+    # Load and properly scale monthly NDVI, merge into a single raster, and crop
     # to the reference grid. It's ok to merge rather than mosaic because MODIS
     # tiles align without overlap.
     ndvi <- lapply(tiles, function(tile) {
@@ -77,8 +94,8 @@ for(satellite in c("aqua", "terra")) {
       ndvi_scale_factor <-
         grep("  scale_factor=", ndvi_metadata) %>%
         ndvi_metadata[.] %>%
-        gsub("  scale_factor=(\\d+)", "\\1", .) %>%
-        as.integer
+        gsub("  scale_factor=(.+)", "\\1", .) %>%
+        as.numeric
 
       # Load the dataset as a raster and apply the scale factor
       # NOTE: the scale factor is applied automatically when loading the raster
@@ -87,42 +104,45 @@ for(satellite in c("aqua", "terra")) {
       # scale the data we need to divide by the square of the scale factor.
       raster(ndvi_dataset) / (ndvi_scale_factor^2)
     }) %>% do.call(merge, .) %>% crop(., grid, snap = "out")
-    # 7 seconds
+    # 10 seconds
     # 9.03 MB
 
-    # Load, merge, and crop monthly NDVI QA data
-    qa <- lapply(tiles, function(tile) {
-      get_subdatasets(tile) %>%
-      .[grepl("1 km monthly pixel reliability$", .)] %>%
-      raster
-    }) %>% do.call(merge, .) %>% crop(., grid, snap = "out")
-    # 5 seconds
-    # 4.52 MB
-
-    # Mask NDVI with the QA data keeping only the highest-quality pixels
-    # TODO look at using lower-quality pixels as well
-    ndvi[qa != 0] <- NA
-
-    # Create a table to hold the extracted data
-    result <- data.frame("modis_grid_id" = grid$id, "date" = basename(date_dir))
-
-    # Extract the data by the reference grid points
+    # Extract the NDVI data by the reference grid points
     # TODO test extracting data by buffers -> slower but more accurate, is the
     # extra accuracy worth the effort?
     result$ndvi <- extract(ndvi, grid)
     # 2 seconds
-    # 10 MB
+    # 15.1 MB
 
-    # Save the exctracted data
+    # Load, merge, and crop monthly NDVI QA data
+    qa <- lapply(tiles, function(tile) {
+      get_subdatasets(tile) %>%
+      .[grepl("1 km monthly VI Quality", .)] %>%
+      raster
+    }) %>% do.call(merge, .) %>% crop(., grid, snap = "out")
+    # 6 seconds
+    # 4.52 MB
+
+    # Extract the QA data by the reference grid points and look up the
+    # corresponding usefulness from the QA codes table
+    result$ndvi_usefulness <-
+      extract(qa, grid) %>%
+      data.frame("code" = .) %>%
+      left_join(., qa_codes[c("code", "usefulness")], by = "code") %>%
+      .$usefulness
+    # 2 seconds
+    # 17.6 MB
+
+    # Save the extracted data
     output_path <-
-      format(date, "%Y-%m") %>%
+      format(d, "%Y-%m") %>%
       paste0("modis_grid_", satellite, "_ndvi_", ., ".rds") %>%
       file.path(year_dir, .)
     saveRDS(result, output_path)
 
     # Return NULL to save memory
     NULL
-  }, mc.cores = ncores)
+  }, mc.cores = ncores, mc.preschedule = FALSE)
 }
 
 report("Done")
