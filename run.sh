0f904ddce4032f331b6a7132c962214a63e0017b
Ian Hough <142520+ihough@users.noreply.github.com>
2017-07-17 18:04:42 +0200

    Cleanup

--- run.sh
+++
@@ -1,6 +0,0 @@
-Rscript ~/temperature-france/preprocess/check_modis_alignment.R
-Rscript ~/temperature-france/preprocess/create_modis_grid.R
-Rscript ~/temperature-france/preprocess/create_modis_buffers.R
-Rscript ~/temperature-france/preprocess/extract_elevation.R
-Rscript ~/temperature-france/preprocess/extract_land_cover.R
-Rscript ~/temperature-france/preprocess/extract_population.R
