c044810fc2ded2554868868286ece8182a51fead
Ian Hough <142520+ihough@users.noreply.github.com>
2017-07-25 14:53:18 +0200

    Extract emissivity along with LST

--- preprocess/extract_modis_lst.R
+++ preprocess/extract_modis_lst.R
@@ -47,13 +47,23 @@ grid <- spTransform(grid, "+proj=sinu +lon_0=0 +x_0=0 +y_0=0 +a=6371007.181 +b=6
 file.path(helpers_dir, "modis_lst_qa_code_table.R") %>% source
 qa_codes <- modis_lst_qa_code_table()
 
+# List the scientific datasets we want to extract
+to_extract <- c(
+  "LST_Day_1km",
+  "LST_Night_1km",
+  "QC_Day",
+  "QC_Night",
+  "Emis_31",
+  "Emis_32"
+)
+
 # Extract Aqua and Terra data in sequence
-for(satellite in c("aqua", "terra")) {
-  paste("Processing MODIS", satellite, "LST data") %>% report
+for(sat in c("aqua", "terra")) {
+  paste("Processing MODIS", sat, "LST data") %>% report
 
   # Set the directory that contains the LST data for the satellite
   lst_dir <-
-    file.path(modis_dir, satellite) %>%
+    file.path(modis_dir, sat) %>%
     list.files(., pattern = "^M.D11A1\\.006$", full.names = TRUE)
 
   # List all months from the first month with data to December 2016
@@ -66,7 +76,7 @@ for(satellite in c("aqua", "terra")) {
   # Iterate over the months
   lapply(months, function(month) {
     # Create a directory to hold the extracted data
-    year_dir <- format(month, "%Y") %>% file.path(extracts_dir, .)
+    year_dir <- format(month, "%Y") %>% file.path(extracts_dir, "modis", .)
     dir.create(year_dir, showWarnings = FALSE)
 
     # List the LST data directories corresponding to each date in the month
@@ -76,89 +86,102 @@ for(satellite in c("aqua", "terra")) {
       seq.Date(month, ., by = 1) %>%
       file.path(lst_dir, .)
 
-    # Extract day and night LST for each date in the month
+    # Extract day and night LST, emissivity, and QA for each date in the month
     # Note that some dates may not have data
-    lapply(c("Day", "Night"), function(tod) {
+    paste("  Extracting", format(month, "%Y-%m"), sat, "LST to", year_dir) %>% report
+    result <- mclapply(date_dirs, function(date_dir) {
+      # Create a template for the extracted data
+      template <- data.frame(
+        "modis_grid_id" = grid$id,
+        "date" = basename(date_dir),
+        "day_lst" = NA,
+        "day_lst_error" = NA,
+        "night_lst" = NA,
+        "night_lst_error" = NA,
+        "emissivity" = NA,
+        "day_emis_error" = NA,
+        "night_emis_error" = NA,
+        stringsAsFactors = FALSE
+      )
+      # 25.1 MB
+
+      # Extract any data for the date to the template
+      if (file.exists(date_dir)) {
+        # Get all tiles for the date
+        tiles <- list.files(date_dir, full.names = TRUE, pattern = "\\.hdf$")
+
+        # Load all datasets of interest from the tiles, merge into a single
+        # stack, and crop to the reference grid. It's ok to merge rather than
+        # mosaic because MODIS tiles align without overlap.
+        datasets <- lapply(tiles, function(tile) {
+          sds <- get_subdatasets(tile)
+          sds_names <- sapply(strsplit(sds, ":"), last)
+          stacked <- sds[sds_names %in% to_extract] %>% stack
+          names(stacked) <- sds_names[sds_names %in% to_extract]
+          stacked
+        })
+        layer_names <- names(datasets[[1]])
+        datasets <- do.call(merge, datasets) %>% crop(., grid, snap = "out")
+        names(datasets) <- layer_names
+        # 16 seconds
+        # 54.1 MB
+
+        # Calculate mean of bands 31 and 32 emissivity
+        datasets$emissivity <- mean(datasets$Emis_31, datasets$Emis_32)
+        datasets <- dropLayer(datasets, c("Emis_31", "Emis_32"))
+        # 45.1 MB
+
+        # Extract the data by the reference grid points
+        # TODO test disaggregating data and extracting by buffers -> slower but
+        # more accurate, is the extra accuracy worth the effort?
+        datasets <- extract(datasets, grid, df = TRUE)
+        # 7 seconds
+        # 30.1 MB
+
+        # Add LST and emissivity to the template
+        template$day_lst    <- datasets$LST_Day_1km
+        template$night_lst  <- datasets$LST_Night_1km
+        template$emissivity <- datasets$emissivity
+        # 32.6 MB
+
+        # Look up the day LST and emissivity error from the QA codes
+        template[c("day_lst_error", "day_emis_error")] <- left_join(
+          data.frame("code" = datasets$QC_Day),
+          qa_codes[c("code", "emis_error", "lst_error")],
+          by = "code"
+        )[c("lst_error", "emis_error")]
+        # 37.7 MB
+
+        # Look up the night LST and emissivity error from the night QA codes
+        template[c("night_lst_error", "night_emis_error")] <- left_join(
+          data.frame("code" = datasets$QC_Night),
+          qa_codes[c("code", "emis_error", "lst_error")],
+          by = "code"
+        )[c("lst_error", "emis_error")]
+        # 42.7 MB
+      }
+      # 27 seconds
+      # 42.7 MB
+
+      # Return the template (which may have no data)
+      template
+    }, mc.cores = ncores) %>% do.call(rbind, .)
+    # 136 seconds (80 for mclapply, 56 for rbind)
+    # 1.28 GB
+
+    # Save the extracted data in fst file format with 75% compression
+    # write.fst is 3x faster than saveRDS
+    output_path <-
       format(month, "%Y-%m") %>%
-        paste("  Extracting", ., satellite, tod, "LST to", year_dir) %>%
-        report
+      paste0("modis_grid_", sat, "_lst_", ., ".fst") %>%
+      file.path(year_dir, .)
+    write.fst(result, output_path, compress = 75)
+    # 24 seconds
 
-      # Extract data for all dates in the month in parallel
-      result <- mclapply(date_dirs, function(date_dir) {
-        # Create a template for the extracted data
-        extracted <- data.frame(
-          "modis_grid_id" = grid$id,
-          "date" = basename(date_dir),
-          "lst" = NA,
-          "lst_error" = NA,
-          stringsAsFactors = FALSE
-        )
-        # 12.6 MB
-
-        # Extract any data for the date to the template
-        if (file.exists(date_dir)) {
-          # Get all tiles for the date
-          tiles <- list.files(date_dir, full.names = TRUE, pattern = "\\.hdf$")
-
-          # Load the LST data from the tiles, merge into a single raster, and
-          # crop to the reference grid. It's ok to merge rather than mosaic
-          # because MODIS tiles align without overlap.
-          lst <- lapply(tiles, function(tile) {
-            get_subdatasets(tile) %>%
-            .[grep(paste("LST", tod, "1km$", sep = "_"), .)] %>%
-            raster
-          }) %>% do.call(merge, .) %>% crop(., grid, snap = "out")
-          # 5 seconds
-          # 9.03 MB
-
-          # Extract the LST data by the reference grid points
-          # TODO test extracting data by buffers -> slower but more accurate, is
-          # the extra accuracy worth the effort?
-          extracted$lst <- extract(lst, grid)
-          # 2 seconds
-
-          # Load, merge, and crop the QA data
-          qa <- lapply(tiles, function(tile) {
-            get_subdatasets(tile) %>%
-            .[grepl(paste("QC", tod, sep = "_"), .)] %>%
-            raster
-          }) %>% do.call(merge, .) %>% crop(., grid, snap = "out")
-          # 4 seconds
-          # 4.52 MB
-
-          # Extract the QA data by the reference grid points and look up the
-          # corresponding LST error from the QA codes table
-          extracted$lst_error <-
-            extract(qa, grid) %>%
-            data.frame("code" = .) %>%
-            left_join(., qa_codes[c("code", "lst_error")], by = "code") %>%
-            .$lst_error
-          # 2 seconds
-        }
-        # 12 seconds
-        # 17.6 MB
-
-        # Return the template (which may have no data)
-        extracted
-      }, mc.cores = ncores, mc.preschedule = FALSE) %>% do.call(rbind, .)
-      # 70 seconds (45 for mclapply, 25 for rbind)
-      # 527 MB
-
-      # Save the extracted data in fst file format with 90% compression
-      # write.fst is 3x faster than saveRDS
-      output_path <-
-        format(month, "%Y-%m") %>%
-        paste0("modis_grid_", satellite, "_lst_", tod, "_", ., ".fst") %>%
-        tolower(.) %>%
-        file.path(year_dir, .)
-      write.fst(result, output_path, compress = 90)
-      # 12 seconds
-
-      # Clear memory
-      rm(result)
-    })
-    # 170 seconds (85 seconds each day / night)
+    # Clear memory
+    rm(result)
   })
+  # 165 seconds per month
 }
 
 report("Done")
